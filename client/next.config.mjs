792a042d2539e7a0557a7f51f9442923c00860de
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2024-07-27 20:20:27 +0530

    rewrite test

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites(){
+        return [
+            {
+                "source": "/blogs", "destination": "https://blogs-plum-ten.vercel.app/blogs"
+            },
+            {
+                "source": "/blogs/:path*", "destination": "https://blogs-plum-ten.vercel.app/blogs/:path*"
+            }
+        ]
+    }
+};
 
 export default nextConfig;
